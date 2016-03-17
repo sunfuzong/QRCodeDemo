434c817b5ec3d7ee95d4fa07745e104bb3decebb
孙富宗 <sunfuzong0@163.com>
2016-03-17 09:13:32 +0800

    Initial Commit

---
+++ QCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QCodeDemo
+//
+//  Created by sunfuzong on 16/3/17.
+//  Copyright © 2016年 sunfuzong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QCodeDemo
+//
+//  Created by sunfuzong on 16/3/17.
+//  Copyright © 2016年 sunfuzong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
